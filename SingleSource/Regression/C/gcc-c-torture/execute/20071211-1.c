981323d31f1afb3ef513569a51aa6779a307eed1
Daniel Thornburgh <mysterymath@gmail.com>
2021-06-19 15:07:58 -0700

    Repair int32 tests.

--- SingleSource/Regression/C/gcc-c-torture/execute/20071211-1.c
+++ SingleSource/Regression/C/gcc-c-torture/execute/20071211-1.c
@@ -1,3 +1,5 @@
+// Modified by LLVM-MOS.
+
 extern void abort() __attribute__ ((noreturn));
 
 struct s
@@ -12,8 +14,8 @@ struct s
 
 int main()
 {
-  int f2;
-  sv.f2 = (1 << 24) - 1;
+  long f2;
+  sv.f2 = (1L << 24) - 1;
   __asm__ volatile ("" : : : "memory");
   ++sv.f2;
   f2 = sv.f2;
--- SingleSource/Regression/C/gcc-c-torture/execute/20141022-1.c
+++ SingleSource/Regression/C/gcc-c-torture/execute/20141022-1.c
@@ -1,9 +1,11 @@
+// Modified by LLVM-MOS.
+
 #define ABORT() do { __builtin_printf("assert.\n");  __builtin_abort (); }while(0)
 int f(int a) __attribute__((noinline));
 int f(int a)
 {
   int fem_key_src;
-  int D2930 = a & 4294967291;
+  long D2930 = a & 4294967291;
   fem_key_src = a == 6 ? 0 : 15;
   fem_key_src = D2930 != 1 ? fem_key_src : 0;
   return fem_key_src;
