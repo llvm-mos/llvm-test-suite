3517bfa5191aa1d3383594aa53ffd90b63ef8d3d
Daniel Thornburgh <mysterymath@gmail.com>
2021-06-19 17:33:29 -0700

    Fix test.

--- SingleSource/UnitTests/2005-07-15-Bitfield-ABI.c
+++ SingleSource/UnitTests/2005-07-15-Bitfield-ABI.c
@@ -1,9 +1,10 @@
+// Modified by LLVM-MOS.
 // PR594
 
 struct X {
   int Q :6;
    int A : 4;
-   int Z : 22;
+   long Z : 22;
 };
 
 void test(struct X *P, int A) {
@@ -14,11 +15,11 @@ extern int printf(const char *str, ...);
 
 int main() {
   union {
-    int Y;
+    long Y;
     struct X Z;
   } U;
   U.Y = ~0;
   test(&U.Z, 0);
-  printf("%x\n", U.Y);
+  printf("%lx\n", U.Y);
   return 0;
 }
