10590087396c54da0766c11b536a2b61d37f3263
github-action-benchmark <github@users.noreply.github.com>
2021-10-01 17:38:41 +0000

    add Benchmark (googlecpp) benchmark result for 48a2c15f68a8d56021c1c173169a25fb14629d50

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1633109912668,
+  "lastUpdate": 1633109921918,
   "repoUrl": "https://github.com/llvm-mos/llvm-test-suite",
   "entries": {
     "Benchmark": [
@@ -23,6 +23,36 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/llvm-mos/llvm-test-suite/commit/48a2c15f68a8d56021c1c173169a25fb14629d50"
         },
         "date": 1633109910700,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "Dhrystone",
+            "value": 404595409,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 404595409 cycles\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "mysterymath@gmail.com",
+            "name": "Daniel Thornburgh",
+            "username": "mysterymath"
+          },
+          "committer": {
+            "email": "mysterymath@gmail.com",
+            "name": "Daniel Thornburgh",
+            "username": "mysterymath"
+          },
+          "distinct": true,
+          "id": "48a2c15f68a8d56021c1c173169a25fb14629d50",
+          "message": "Remove cat from bench.",
+          "timestamp": "2021-10-01T10:33:13-07:00",
+          "tree_id": "2c17268ff0757eb427a7c9e674a55d8e6c7c5bf5",
+          "url": "https://github.com/llvm-mos/llvm-test-suite/commit/48a2c15f68a8d56021c1c173169a25fb14629d50"
+        },
+        "date": 1633109920676,
         "tool": "googlecpp",
         "benches": [
           {
