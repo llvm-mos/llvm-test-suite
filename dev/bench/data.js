2673cc42dfccc6518632826439f34e8c2e76b283
github-action-benchmark <github@users.noreply.github.com>
2021-10-02 03:55:46 +0000

    add Benchmark -Os -Wl,-mllvm,-num-imag-ptrs=16 (googlecpp) benchmark result for 8a9d9f43a8645b7ad854df418d5575e6cec5b969

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1633110692653,
+  "lastUpdate": 1633146946843,
   "repoUrl": "https://github.com/llvm-mos/llvm-test-suite",
   "entries": {
     "Benchmark -Os": [
@@ -55,6 +55,36 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/llvm-mos/llvm-test-suite/commit/96b5545f56ebd140ade37e2f6de5116aa4e207f1"
         },
         "date": 1633110690904,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "Dhrystone",
+            "value": 404595409,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 404595409 cycles\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "mysterymath@gmail.com",
+            "name": "Daniel Thornburgh",
+            "username": "mysterymath"
+          },
+          "committer": {
+            "email": "mysterymath@gmail.com",
+            "name": "Daniel Thornburgh",
+            "username": "mysterymath"
+          },
+          "distinct": true,
+          "id": "8a9d9f43a8645b7ad854df418d5575e6cec5b969",
+          "message": "Add benchmark action to test suite project.\n\nThis only runs Dhrystone for now, but it runs nightly and charts the\nresults in a nice webpage (https://llvm-mos.github.io/llvm-test-suite).",
+          "timestamp": "2021-10-01T20:49:57-07:00",
+          "tree_id": "4643b1d2e34c34cfbe59032dd948e7c2eb865634",
+          "url": "https://github.com/llvm-mos/llvm-test-suite/commit/8a9d9f43a8645b7ad854df418d5575e6cec5b969"
+        },
+        "date": 1633146945447,
         "tool": "googlecpp",
         "benches": [
           {
