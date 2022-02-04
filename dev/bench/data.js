4906c44863e668c66f53c8ec319d3d4bf12c12d6
github-action-benchmark <github@users.noreply.github.com>
2022-02-04 22:10:23 +0000

    add Benchmark -Os (googlecpp) benchmark result for c43060686519d98e56b92c0453c1e2704971569b

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1643966938426,
+  "lastUpdate": 1644012623435,
   "repoUrl": "https://github.com/llvm-mos/llvm-test-suite",
   "entries": {
     "Benchmark -Os": [
@@ -14402,6 +14402,90 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/llvm-mos/llvm-mos/commit/481972aa06925c4b7945ed8b8e8868b329490446"
         },
         "date": 1643966936197,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "6502-compilers/bench/ccgame/game_01_start",
+            "value": 3839101,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 3839101 cycles\nthreads: 1"
+          },
+          {
+            "name": "6502-compilers/bench/ccgame/game_modern_optims",
+            "value": 813949,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 813949 cycles\nthreads: 1"
+          },
+          {
+            "name": "6502-compilers/bench/ccgame/game_modern_optims_structarray",
+            "value": 2532402,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 2532402 cycles\nthreads: 1"
+          },
+          {
+            "name": "6502-compilers/bench/coroutine",
+            "value": 7374,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 7374 cycles\nthreads: 1"
+          },
+          {
+            "name": "6502-compilers/bench/memcpy",
+            "value": 16582,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 16582 cycles\nthreads: 1"
+          },
+          {
+            "name": "6502-compilers/bench/rpg",
+            "value": 159,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 159 cycles\nthreads: 1"
+          },
+          {
+            "name": "6502-compilers/bench/unzip",
+            "value": 39293,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 39293 cycles\nthreads: 1"
+          },
+          {
+            "name": "Dhrystone",
+            "value": 292506719,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 292506719 cycles\nthreads: 1"
+          },
+          {
+            "name": "BYTE Sieve",
+            "value": 21302531,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 21302531 cycles\nthreads: 1"
+          },
+          {
+            "name": "CoreMark",
+            "value": 126,
+            "unit": "sec/iter",
+            "extra": "iterations: 10\ncpu: 126 sec\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "mysterymath@gmail.com",
+            "name": "Daniel Thornburgh",
+            "username": "mysterymath"
+          },
+          "committer": {
+            "email": "mysterymath@gmail.com",
+            "name": "Daniel Thornburgh",
+            "username": "mysterymath"
+          },
+          "distinct": true,
+          "id": "c43060686519d98e56b92c0453c1e2704971569b",
+          "message": "Don't issue the zeropage indexed addr modes; they wrap.",
+          "timestamp": "2022-02-04T10:38:03-08:00",
+          "tree_id": "240fa120a79a902978a70e911eef9ccb239a51e6",
+          "url": "https://github.com/llvm-mos/llvm-mos/commit/c43060686519d98e56b92c0453c1e2704971569b"
+        },
+        "date": 1644012621200,
         "tool": "googlecpp",
         "benches": [
           {
