c7459f89a675735b21d53c3985975cf25d839c9c
github-action-benchmark <github@users.noreply.github.com>
2022-02-08 00:32:39 +0000

    add Benchmark -Os (googlecpp) benchmark result for 78769d8ec9832c31bfdca0ea9286c7e3f5abde65

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,6 +1,6 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1644221372238,
-  "repoUrl": "",
+  "lastUpdate": 1644280359563,
+  "repoUrl": "https://github.com/llvm-mos/llvm-test-suite",
   "entries": {
     "Benchmark -Os": [
       {
@@ -14810,6 +14810,90 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/llvm-mos/llvm-test-suite/commit/d2e8baf8967e77c1b3bbb4259e950f8915fdb917"
         },
         "date": 1644221370807,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "6502-compilers/bench/ccgame/game_01_start",
+            "value": 3839101,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 3839101 cycles\nthreads: 1"
+          },
+          {
+            "name": "6502-compilers/bench/ccgame/game_modern_optims",
+            "value": 813949,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 813949 cycles\nthreads: 1"
+          },
+          {
+            "name": "6502-compilers/bench/ccgame/game_modern_optims_structarray",
+            "value": 2532402,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 2532402 cycles\nthreads: 1"
+          },
+          {
+            "name": "6502-compilers/bench/coroutine",
+            "value": 7374,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 7374 cycles\nthreads: 1"
+          },
+          {
+            "name": "6502-compilers/bench/memcpy",
+            "value": 16582,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 16582 cycles\nthreads: 1"
+          },
+          {
+            "name": "6502-compilers/bench/rpg",
+            "value": 159,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 159 cycles\nthreads: 1"
+          },
+          {
+            "name": "6502-compilers/bench/unzip",
+            "value": 39293,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 39293 cycles\nthreads: 1"
+          },
+          {
+            "name": "Dhrystone",
+            "value": 292506719,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 292506719 cycles\nthreads: 1"
+          },
+          {
+            "name": "BYTE Sieve",
+            "value": 21302531,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 21302531 cycles\nthreads: 1"
+          },
+          {
+            "name": "CoreMark",
+            "value": 126,
+            "unit": "sec/iter",
+            "extra": "iterations: 10\ncpu: 126 sec\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "kevin.arunski@gmail.com",
+            "name": "karunski",
+            "username": "karunski"
+          },
+          "committer": {
+            "email": "mysterymath@gmail.com",
+            "name": "Daniel Thornburgh",
+            "username": "mysterymath"
+          },
+          "distinct": true,
+          "id": "78769d8ec9832c31bfdca0ea9286c7e3f5abde65",
+          "message": "The definition of USHRT_MAX does not fit in an int.\n\nThis is a side effect of having sizeof(int) == sizeof(short).",
+          "timestamp": "2022-02-07T13:04:06-08:00",
+          "tree_id": "d5c0a1db6e401b85f4a5b4a966220d4bf35f9d26",
+          "url": "https://github.com/llvm-mos/llvm-mos/commit/78769d8ec9832c31bfdca0ea9286c7e3f5abde65"
+        },
+        "date": 1644280357972,
         "tool": "googlecpp",
         "benches": [
           {
