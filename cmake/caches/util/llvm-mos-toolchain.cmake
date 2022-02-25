453f2bbeefcaeac6db981169d439dc62228b4b90
Daniel Thornburgh <mysterymath@gmail.com>
2022-02-24 16:51:37 -0800

    Try adding extension on Windows.

--- cmake/caches/util/llvm-mos-toolchain.cmake
+++ cmake/caches/util/llvm-mos-toolchain.cmake
@@ -4,6 +4,9 @@ cmake_minimum_required(VERSION 3.13)
 set(CMAKE_SYSTEM_NAME Generic)
 
 # Find mos compilers if not already set.
-set(CMAKE_C_COMPILER ${LLVM_MOS}/bin/mos-sim-clang CACHE STRING "")
-set(CMAKE_CXX_COMPILER ${LLVM_MOS}/bin/mos-sim-clang++ CACHE STRING "")
-set(CMAKE_ASM_COMPILER ${LLVM_MOS}/bin/mos-sim-clang CACHE STRING "")
+if(WIN32)
+  set(extension ".exe")
+endif()
+set(CMAKE_C_COMPILER ${LLVM_MOS}/bin/mos-sim-clang${extension} CACHE STRING "")
+set(CMAKE_CXX_COMPILER ${LLVM_MOS}/bin/mos-sim-clang++${extension} CACHE STRING "")
+set(CMAKE_ASM_COMPILER ${LLVM_MOS}/bin/mos-sim-clang${extension} CACHE STRING "")
