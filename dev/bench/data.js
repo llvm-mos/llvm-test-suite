dacd47731b376d6c0f10daba2e47d7e9cb163863
github-action-benchmark <github@users.noreply.github.com>
2022-03-25 03:01:42 +0000

    add Benchmark -Os (googlecpp) benchmark result for 0ceee02e65be164f1acdfde4757dbb593ed2e80a

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1648157575182,
+  "lastUpdate": 1648177302303,
   "repoUrl": "https://github.com/llvm-mos/llvm-test-suite",
   "entries": {
     "Benchmark -Os": [
@@ -22311,6 +22311,90 @@ window.BENCHMARK_DATA = {
             "value": 288490840,
             "unit": "cycles/iter",
             "extra": "iterations: 1\ncpu: 288490840 cycles\nthreads: 1"
+          },
+          {
+            "name": "BYTE Sieve",
+            "value": 19914202,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 19914202 cycles\nthreads: 1"
+          },
+          {
+            "name": "CoreMark",
+            "value": 124,
+            "unit": "sec/iter",
+            "extra": "iterations: 10\ncpu: 124 sec\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "mysterymath@gmail.com",
+            "name": "Daniel Thornburgh",
+            "username": "mysterymath"
+          },
+          "committer": {
+            "email": "mysterymath@gmail.com",
+            "name": "Daniel Thornburgh",
+            "username": "mysterymath"
+          },
+          "distinct": true,
+          "id": "0ceee02e65be164f1acdfde4757dbb593ed2e80a",
+          "message": "Fix divmod naming.",
+          "timestamp": "2022-03-24T19:43:18-07:00",
+          "tree_id": "066264780f6f5a1cc9d167238acbb7e9c92bab59",
+          "url": "https://github.com/llvm-mos/llvm-mos-sdk/commit/0ceee02e65be164f1acdfde4757dbb593ed2e80a"
+        },
+        "date": 1648177299537,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "6502-compilers/bench/ccgame/game_01_start",
+            "value": 3753119,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 3753119 cycles\nthreads: 1"
+          },
+          {
+            "name": "6502-compilers/bench/ccgame/game_modern_optims",
+            "value": 813904,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 813904 cycles\nthreads: 1"
+          },
+          {
+            "name": "6502-compilers/bench/ccgame/game_modern_optims_structarray",
+            "value": 2510966,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 2510966 cycles\nthreads: 1"
+          },
+          {
+            "name": "6502-compilers/bench/coroutine",
+            "value": 6876,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 6876 cycles\nthreads: 1"
+          },
+          {
+            "name": "6502-compilers/bench/memcpy",
+            "value": 16596,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 16596 cycles\nthreads: 1"
+          },
+          {
+            "name": "6502-compilers/bench/rpg",
+            "value": 173,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 173 cycles\nthreads: 1"
+          },
+          {
+            "name": "6502-compilers/bench/unzip",
+            "value": 76510,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 76510 cycles\nthreads: 1"
+          },
+          {
+            "name": "Dhrystone",
+            "value": 288490830,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 288490830 cycles\nthreads: 1"
           },
           {
             "name": "BYTE Sieve",
