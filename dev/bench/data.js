89ba060a0fb4daff2857371f610e667c41b09ba4
github-action-benchmark <github@users.noreply.github.com>
2022-07-12 16:31:09 +0000

    add Benchmark -Os (googlecpp) benchmark result for 10de99556909fc7c1e85584d8bcf572193573710

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,6 +1,6 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1657613902176,
-  "repoUrl": "",
+  "lastUpdate": 1657643469876,
+  "repoUrl": "https://github.com/llvm-mos/llvm-test-suite",
   "entries": {
     "Benchmark -Os": [
       {
@@ -36886,6 +36886,90 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/llvm-mos/llvm-test-suite/commit/cd463bddc2324c4d36437abea8999ab325935bfb"
         },
         "date": 1657613899250,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "6502-compilers/bench/ccgame/game_01_start",
+            "value": 3534104,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 3534104 cycles\nthreads: 1"
+          },
+          {
+            "name": "6502-compilers/bench/ccgame/game_modern_optims",
+            "value": 683856,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 683856 cycles\nthreads: 1"
+          },
+          {
+            "name": "6502-compilers/bench/ccgame/game_modern_optims_structarray",
+            "value": 2378680,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 2378680 cycles\nthreads: 1"
+          },
+          {
+            "name": "6502-compilers/bench/coroutine",
+            "value": 6876,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 6876 cycles\nthreads: 1"
+          },
+          {
+            "name": "6502-compilers/bench/memcpy",
+            "value": 10136,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 10136 cycles\nthreads: 1"
+          },
+          {
+            "name": "6502-compilers/bench/rpg",
+            "value": 167,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 167 cycles\nthreads: 1"
+          },
+          {
+            "name": "6502-compilers/bench/unzip",
+            "value": 44832,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 44832 cycles\nthreads: 1"
+          },
+          {
+            "name": "Dhrystone",
+            "value": 284490599,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 284490599 cycles\nthreads: 1"
+          },
+          {
+            "name": "BYTE Sieve",
+            "value": 21418264,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 21418264 cycles\nthreads: 1"
+          },
+          {
+            "name": "CoreMark",
+            "value": 114,
+            "unit": "sec/iter",
+            "extra": "iterations: 10\ncpu: 114 sec\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "mysterymath@gmail.com",
+            "name": "Daniel Thornburgh",
+            "username": "mysterymath"
+          },
+          "committer": {
+            "email": "mysterymath@gmail.com",
+            "name": "Daniel Thornburgh",
+            "username": "mysterymath"
+          },
+          "distinct": true,
+          "id": "10de99556909fc7c1e85584d8bcf572193573710",
+          "message": "Make atari8 exit by returning again.\n\nEven if we clobber BASIC, that doesn't mean we can't return to DOS,\nwhich is typically where you run ASM programs from on Atari 8-bit.",
+          "timestamp": "2022-07-12T09:12:31-07:00",
+          "tree_id": "a77b9da391bcc963bb20cb0321c02c7718b03af2",
+          "url": "https://github.com/llvm-mos/llvm-mos-sdk/commit/10de99556909fc7c1e85584d8bcf572193573710"
+        },
+        "date": 1657643465661,
         "tool": "googlecpp",
         "benches": [
           {
