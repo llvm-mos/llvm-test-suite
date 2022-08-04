8c39261f9c1ff3c52a1037e9cd87dadcaab3079f
github-action-benchmark <github@users.noreply.github.com>
2022-08-04 05:26:50 +0000

    add Benchmark -Os (googlecpp) benchmark result for 69b3b8346a10a4128b8bed419078dffe10502d0a

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1659553596002,
+  "lastUpdate": 1659590810907,
   "repoUrl": "https://github.com/llvm-mos/llvm-test-suite",
   "entries": {
     "Benchmark -Os": [
@@ -40690,6 +40690,90 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/llvm-mos/llvm-test-suite/commit/cd463bddc2324c4d36437abea8999ab325935bfb"
         },
         "date": 1659553591137,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "6502-compilers/bench/ccgame/game_01_start",
+            "value": 3532957,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 3532957 cycles\nthreads: 1"
+          },
+          {
+            "name": "6502-compilers/bench/ccgame/game_modern_optims",
+            "value": 675989,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 675989 cycles\nthreads: 1"
+          },
+          {
+            "name": "6502-compilers/bench/ccgame/game_modern_optims_structarray",
+            "value": 2404196,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 2404196 cycles\nthreads: 1"
+          },
+          {
+            "name": "6502-compilers/bench/coroutine",
+            "value": 6364,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 6364 cycles\nthreads: 1"
+          },
+          {
+            "name": "6502-compilers/bench/memcpy",
+            "value": 10136,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 10136 cycles\nthreads: 1"
+          },
+          {
+            "name": "6502-compilers/bench/rpg",
+            "value": 165,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 165 cycles\nthreads: 1"
+          },
+          {
+            "name": "6502-compilers/bench/unzip",
+            "value": 44832,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 44832 cycles\nthreads: 1"
+          },
+          {
+            "name": "Dhrystone",
+            "value": 284490599,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 284490599 cycles\nthreads: 1"
+          },
+          {
+            "name": "BYTE Sieve",
+            "value": 21558366,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 21558366 cycles\nthreads: 1"
+          },
+          {
+            "name": "CoreMark",
+            "value": 113,
+            "unit": "sec/iter",
+            "extra": "iterations: 10\ncpu: 113 sec\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "mysterymath@gmail.com",
+            "name": "Daniel Thornburgh",
+            "username": "mysterymath"
+          },
+          "committer": {
+            "email": "mysterymath@gmail.com",
+            "name": "Daniel Thornburgh",
+            "username": "mysterymath"
+          },
+          "distinct": true,
+          "id": "69b3b8346a10a4128b8bed419078dffe10502d0a",
+          "message": "Port cc65 hardware register definitions for atari8.\n\nSee issue #61.",
+          "timestamp": "2022-08-03T22:10:43-07:00",
+          "tree_id": "73b5488d4b81a6538047e6b0236bf36861bf21f2",
+          "url": "https://github.com/llvm-mos/llvm-mos-sdk/commit/69b3b8346a10a4128b8bed419078dffe10502d0a"
+        },
+        "date": 1659590807726,
         "tool": "googlecpp",
         "benches": [
           {
