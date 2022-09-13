7af3b06f273307b8de391c8d0e4f2f613b7bae06
github-action-benchmark <github@users.noreply.github.com>
2022-09-13 00:47:25 +0000

    add Benchmark -Os (googlecpp) benchmark result for 5356407fa0a6699013fe05c11c69641ea00fc38e

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1663017057738,
+  "lastUpdate": 1663030044988,
   "repoUrl": "https://github.com/llvm-mos/llvm-test-suite",
   "entries": {
     "Benchmark -Os": [
@@ -48354,6 +48354,90 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/llvm-mos/llvm-mos-sdk/commit/4d2c807476700af1cd4b5927f15d21db7f15bef3"
         },
         "date": 1663017053730,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "6502-compilers/bench/ccgame/game_01_start",
+            "value": 3452882,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 3452882 cycles\nthreads: 1"
+          },
+          {
+            "name": "6502-compilers/bench/ccgame/game_modern_optims",
+            "value": 676022,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 676022 cycles\nthreads: 1"
+          },
+          {
+            "name": "6502-compilers/bench/ccgame/game_modern_optims_structarray",
+            "value": 2241145,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 2241145 cycles\nthreads: 1"
+          },
+          {
+            "name": "6502-compilers/bench/coroutine",
+            "value": 6364,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 6364 cycles\nthreads: 1"
+          },
+          {
+            "name": "6502-compilers/bench/memcpy",
+            "value": 10136,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 10136 cycles\nthreads: 1"
+          },
+          {
+            "name": "6502-compilers/bench/rpg",
+            "value": 165,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 165 cycles\nthreads: 1"
+          },
+          {
+            "name": "6502-compilers/bench/unzip",
+            "value": 44832,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 44832 cycles\nthreads: 1"
+          },
+          {
+            "name": "Dhrystone",
+            "value": 284490593,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 284490593 cycles\nthreads: 1"
+          },
+          {
+            "name": "BYTE Sieve",
+            "value": 20831957,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 20831957 cycles\nthreads: 1"
+          },
+          {
+            "name": "CoreMark",
+            "value": 112,
+            "unit": "sec/iter",
+            "extra": "iterations: 10\ncpu: 112 sec\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "mysterymath@gmail.com",
+            "name": "Daniel Thornburgh",
+            "username": "mysterymath"
+          },
+          "committer": {
+            "email": "mysterymath@gmail.com",
+            "name": "Daniel Thornburgh",
+            "username": "mysterymath"
+          },
+          "distinct": true,
+          "id": "5356407fa0a6699013fe05c11c69641ea00fc38e",
+          "message": "[neslib] [nesdoug] Add post_vram_update hook to clear VRAM_BUF.",
+          "timestamp": "2022-09-12T17:23:58-07:00",
+          "tree_id": "5688f8e5c5202d4c315b8bce4706a86fbbfaabd5",
+          "url": "https://github.com/llvm-mos/llvm-mos-sdk/commit/5356407fa0a6699013fe05c11c69641ea00fc38e"
+        },
+        "date": 1663030041609,
         "tool": "googlecpp",
         "benches": [
           {
