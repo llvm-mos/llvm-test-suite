fce41d9451c6308ccc2615f38c7a51ca3886b9a7
github-action-benchmark <github@users.noreply.github.com>
2023-09-14 19:19:03 +0000

    add Benchmark -Os (googlecpp) benchmark result for 27c49bcc273a88b0a75fb59d28417bad69233550

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1694717448144,
+  "lastUpdate": 1694719143765,
   "repoUrl": "https://github.com/llvm-mos/llvm-test-suite",
   "entries": {
     "Benchmark -Os": [
@@ -94206,6 +94206,90 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/llvm-mos/llvm-mos-sdk/commit/2652bb048597e26adb378857aa86de56a73f9b35"
         },
         "date": 1694717442510,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "6502-compilers/bench/ccgame/game_01_start",
+            "value": 3085193,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 3085193 cycles\nthreads: 1"
+          },
+          {
+            "name": "6502-compilers/bench/ccgame/game_modern_optims",
+            "value": 647635,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 647635 cycles\nthreads: 1"
+          },
+          {
+            "name": "6502-compilers/bench/ccgame/game_modern_optims_structarray",
+            "value": 2098427,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 2098427 cycles\nthreads: 1"
+          },
+          {
+            "name": "6502-compilers/bench/coroutine",
+            "value": 6946,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 6946 cycles\nthreads: 1"
+          },
+          {
+            "name": "6502-compilers/bench/memcpy",
+            "value": 10146,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 10146 cycles\nthreads: 1"
+          },
+          {
+            "name": "6502-compilers/bench/rpg",
+            "value": 175,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 175 cycles\nthreads: 1"
+          },
+          {
+            "name": "6502-compilers/bench/unzip",
+            "value": 44845,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 44845 cycles\nthreads: 1"
+          },
+          {
+            "name": "Dhrystone",
+            "value": 40327821,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 40327821 cycles\nthreads: 1"
+          },
+          {
+            "name": "BYTE Sieve",
+            "value": 21249917,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 21249917 cycles\nthreads: 1"
+          },
+          {
+            "name": "CoreMark",
+            "value": 107,
+            "unit": "sec/iter",
+            "extra": "iterations: 10\ncpu: 107 sec\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "mysterymath@gmail.com",
+            "name": "Daniel Thornburgh",
+            "username": "mysterymath"
+          },
+          "committer": {
+            "email": "mysterymath@gmail.com",
+            "name": "Daniel Thornburgh",
+            "username": "mysterymath"
+          },
+          "distinct": true,
+          "id": "27c49bcc273a88b0a75fb59d28417bad69233550",
+          "message": "[Breaking] Remove size-specific linker scripts from MMC1\n\nMMC1 can now arrange PRG-ROM using only the value of the\n`__prg_rom_size` symbol; it's no longer necessary to include a custom\nlinker script to set the size. Custom linker script inclusion is still\nnecessary to set the banking mode.\n\nAs part of this, the behavior of the 32-KiB banked mode is no longer\nspecial-cased to initialize the bank to zero. This gives it the same\nindeterminate startup behavior as the other banked modes; this\ninitialization must be done by hand.",
+          "timestamp": "2023-09-14T11:51:52-07:00",
+          "tree_id": "ff0909cf7adb75456bcdb1d454326bebf1a3b92c",
+          "url": "https://github.com/llvm-mos/llvm-mos-sdk/commit/27c49bcc273a88b0a75fb59d28417bad69233550"
+        },
+        "date": 1694719137921,
         "tool": "googlecpp",
         "benches": [
           {
