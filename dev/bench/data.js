fcfd70dedb016328bbebc792a6ecd8f161ea5a32
github-action-benchmark <github@users.noreply.github.com>
2023-09-17 01:37:27 +0000

    add Benchmark -Os (googlecpp) benchmark result for a97423b4a31231bc59cf92074132f7eeb0316888

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1694908508848,
+  "lastUpdate": 1694914647204,
   "repoUrl": "https://github.com/llvm-mos/llvm-test-suite",
   "entries": {
     "Benchmark -Os": [
@@ -95038,6 +95038,90 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/llvm-mos/llvm-mos-sdk/commit/bae6f5ca66d2c8ad6b9ed9fa9f1f0bfdd604868a"
         },
         "date": 1694908503074,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "6502-compilers/bench/ccgame/game_01_start",
+            "value": 3085193,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 3085193 cycles\nthreads: 1"
+          },
+          {
+            "name": "6502-compilers/bench/ccgame/game_modern_optims",
+            "value": 647635,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 647635 cycles\nthreads: 1"
+          },
+          {
+            "name": "6502-compilers/bench/ccgame/game_modern_optims_structarray",
+            "value": 2098427,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 2098427 cycles\nthreads: 1"
+          },
+          {
+            "name": "6502-compilers/bench/coroutine",
+            "value": 6946,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 6946 cycles\nthreads: 1"
+          },
+          {
+            "name": "6502-compilers/bench/memcpy",
+            "value": 10146,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 10146 cycles\nthreads: 1"
+          },
+          {
+            "name": "6502-compilers/bench/rpg",
+            "value": 175,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 175 cycles\nthreads: 1"
+          },
+          {
+            "name": "6502-compilers/bench/unzip",
+            "value": 44845,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 44845 cycles\nthreads: 1"
+          },
+          {
+            "name": "Dhrystone",
+            "value": 40327821,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 40327821 cycles\nthreads: 1"
+          },
+          {
+            "name": "BYTE Sieve",
+            "value": 21249917,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 21249917 cycles\nthreads: 1"
+          },
+          {
+            "name": "CoreMark",
+            "value": 107,
+            "unit": "sec/iter",
+            "extra": "iterations: 10\ncpu: 107 sec\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "mysterymath@gmail.com",
+            "name": "Daniel Thornburgh",
+            "username": "mysterymath"
+          },
+          "committer": {
+            "email": "mysterymath@gmail.com",
+            "name": "Daniel Thornburgh",
+            "username": "mysterymath"
+          },
+          "distinct": true,
+          "id": "a97423b4a31231bc59cf92074132f7eeb0316888",
+          "message": "Revert \"[Breaking] Rework MMC1 banking. (#204)\"\n\nThis reverts commit dc848e7226163ae345cca210d44f18c04e50f1d8.\n\nThis generally needs more work; I have accidentally introduced C into\nthe NMI path, and it may use RC registers, which aren't saved by the NMI\nprologue/epilogue. It's also become clear that I don't have a good model\nfor how reentrant these routines need to be.",
+          "timestamp": "2023-09-16T18:24:21-07:00",
+          "tree_id": "12baa019613142d7b2250efc1c18fa0f17a0e279",
+          "url": "https://github.com/llvm-mos/llvm-mos-sdk/commit/a97423b4a31231bc59cf92074132f7eeb0316888"
+        },
+        "date": 1694914639833,
         "tool": "googlecpp",
         "benches": [
           {
