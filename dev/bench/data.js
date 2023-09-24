d2bf0d9cb36052c110d52c7a3f160e1da575f75d
github-action-benchmark <github@users.noreply.github.com>
2023-09-24 23:26:49 +0000

    add Benchmark -Os (googlecpp) benchmark result for bdcf0f5838d6037bfd6253744acb23507f1f14d8

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1695596511920,
+  "lastUpdate": 1695598009752,
   "repoUrl": "https://github.com/llvm-mos/llvm-test-suite",
   "entries": {
     "Benchmark -Os": [
@@ -96590,6 +96590,90 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/llvm-mos/llvm-mos-sdk/commit/55051ddf16e70679c48c7318dbd6bb8b17f9f7bf"
         },
         "date": 1695596504561,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "6502-compilers/bench/ccgame/game_01_start",
+            "value": 3085193,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 3085193 cycles\nthreads: 1"
+          },
+          {
+            "name": "6502-compilers/bench/ccgame/game_modern_optims",
+            "value": 647635,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 647635 cycles\nthreads: 1"
+          },
+          {
+            "name": "6502-compilers/bench/ccgame/game_modern_optims_structarray",
+            "value": 2098427,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 2098427 cycles\nthreads: 1"
+          },
+          {
+            "name": "6502-compilers/bench/coroutine",
+            "value": 6946,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 6946 cycles\nthreads: 1"
+          },
+          {
+            "name": "6502-compilers/bench/memcpy",
+            "value": 10146,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 10146 cycles\nthreads: 1"
+          },
+          {
+            "name": "6502-compilers/bench/rpg",
+            "value": 175,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 175 cycles\nthreads: 1"
+          },
+          {
+            "name": "6502-compilers/bench/unzip",
+            "value": 44845,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 44845 cycles\nthreads: 1"
+          },
+          {
+            "name": "Dhrystone",
+            "value": 40327821,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 40327821 cycles\nthreads: 1"
+          },
+          {
+            "name": "BYTE Sieve",
+            "value": 21249917,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 21249917 cycles\nthreads: 1"
+          },
+          {
+            "name": "CoreMark",
+            "value": 107,
+            "unit": "sec/iter",
+            "extra": "iterations: 10\ncpu: 107 sec\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "cogwheel@users.noreply.github.com",
+            "name": "Matthew Orlando",
+            "username": "cogwheel"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "bdcf0f5838d6037bfd6253744acb23507f1f14d8",
+          "message": "Split neslib/nesdoug, take 2 (#221)\n\n* Un-revert \"Split neslib and nesdoug components so they are linked conditionally (#217)\"\r\n\r\nThis reverts commit 2207d5a046747c3cd116bbb37cf2f6e00fbcb484.\r\n\r\n* Fix immediate death in nesdoug full game",
+          "timestamp": "2023-09-24T16:13:20-07:00",
+          "tree_id": "6a6eb49b30615d9d655893dc4995f15836682d2b",
+          "url": "https://github.com/llvm-mos/llvm-mos-sdk/commit/bdcf0f5838d6037bfd6253744acb23507f1f14d8"
+        },
+        "date": 1695598003019,
         "tool": "googlecpp",
         "benches": [
           {
