b3586eae7dffdd046862f605e84794e06ea6a3b5
github-action-benchmark <github@users.noreply.github.com>
2023-11-12 21:50:24 +0000

    add Benchmark -Os (googlecpp) benchmark result for d9b33734a7c78352d373b737e9092e0420f297c0

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1699776633004,
+  "lastUpdate": 1699825824387,
   "repoUrl": "https://github.com/llvm-mos/llvm-test-suite",
   "entries": {
     "Benchmark -Os": [
@@ -103210,6 +103210,90 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/llvm-mos/llvm-test-suite/commit/530a35c099095f1cd1eeead6df120ceb12fda47d"
         },
         "date": 1699776628201,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "6502-compilers/bench/ccgame/game_01_start",
+            "value": 3079153,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 3079153 cycles\nthreads: 1"
+          },
+          {
+            "name": "6502-compilers/bench/ccgame/game_modern_optims",
+            "value": 647635,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 647635 cycles\nthreads: 1"
+          },
+          {
+            "name": "6502-compilers/bench/ccgame/game_modern_optims_structarray",
+            "value": 2098427,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 2098427 cycles\nthreads: 1"
+          },
+          {
+            "name": "6502-compilers/bench/coroutine",
+            "value": 6946,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 6946 cycles\nthreads: 1"
+          },
+          {
+            "name": "6502-compilers/bench/memcpy",
+            "value": 10146,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 10146 cycles\nthreads: 1"
+          },
+          {
+            "name": "6502-compilers/bench/rpg",
+            "value": 175,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 175 cycles\nthreads: 1"
+          },
+          {
+            "name": "6502-compilers/bench/unzip",
+            "value": 34546,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 34546 cycles\nthreads: 1"
+          },
+          {
+            "name": "Dhrystone",
+            "value": 36312197,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 36312197 cycles\nthreads: 1"
+          },
+          {
+            "name": "BYTE Sieve",
+            "value": 20348900,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 20348900 cycles\nthreads: 1"
+          },
+          {
+            "name": "CoreMark",
+            "value": 107,
+            "unit": "sec/iter",
+            "extra": "iterations: 10\ncpu: 107 sec\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "sehugg@users.noreply.github.com",
+            "name": "Steven Hugg",
+            "username": "sehugg"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "d9b33734a7c78352d373b737e9092e0420f297c0",
+          "message": "[Atari 2600] Bug fixes: (#239)\n\n- Fixed zp section overlapping registers.\r\n- Fixed bank selection macros to accept macro parameters.\r\n- Better Stella signature.",
+          "timestamp": "2023-11-12T13:40:30-08:00",
+          "tree_id": "488bfe121ba0d878dcaf1b6d696562baa9f11807",
+          "url": "https://github.com/llvm-mos/llvm-mos-sdk/commit/d9b33734a7c78352d373b737e9092e0420f297c0"
+        },
+        "date": 1699825818559,
         "tool": "googlecpp",
         "benches": [
           {
