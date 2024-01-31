42aaffcd162043f8e6ee119b9c5a904c547a83bc
github-action-benchmark <github@users.noreply.github.com>
2024-01-31 18:34:53 +0000

    add Benchmark -Os (googlecpp) benchmark result for 72e337959d136a5971ad1c9c2eee7e933df0f2b2

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1706688693876,
+  "lastUpdate": 1706726093335,
   "repoUrl": "https://github.com/llvm-mos/llvm-test-suite",
   "entries": {
     "Benchmark -Os": [
@@ -118314,6 +118314,90 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/llvm-mos/llvm-test-suite/commit/b46fe2315bff130dc97eb149e6fa2f2c0c983452"
         },
         "date": 1706688688236,
+        "tool": "googlecpp",
+        "benches": [
+          {
+            "name": "6502-compilers/bench/ccgame/game_01_start",
+            "value": 2311776,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 2311776 cycles\nthreads: 1"
+          },
+          {
+            "name": "6502-compilers/bench/ccgame/game_modern_optims",
+            "value": 645502,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 645502 cycles\nthreads: 1"
+          },
+          {
+            "name": "6502-compilers/bench/ccgame/game_modern_optims_structarray",
+            "value": 1495661,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 1495661 cycles\nthreads: 1"
+          },
+          {
+            "name": "6502-compilers/bench/coroutine",
+            "value": 8679,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 8679 cycles\nthreads: 1"
+          },
+          {
+            "name": "6502-compilers/bench/memcpy",
+            "value": 10146,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 10146 cycles\nthreads: 1"
+          },
+          {
+            "name": "6502-compilers/bench/rpg",
+            "value": 175,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 175 cycles\nthreads: 1"
+          },
+          {
+            "name": "6502-compilers/bench/unzip",
+            "value": 34546,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 34546 cycles\nthreads: 1"
+          },
+          {
+            "name": "Dhrystone",
+            "value": 36312197,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 36312197 cycles\nthreads: 1"
+          },
+          {
+            "name": "BYTE Sieve",
+            "value": 24757550,
+            "unit": "cycles/iter",
+            "extra": "iterations: 1\ncpu: 24757550 cycles\nthreads: 1"
+          },
+          {
+            "name": "CoreMark",
+            "value": 108,
+            "unit": "sec/iter",
+            "extra": "iterations: 10\ncpu: 108 sec\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "mysterymath@gmail.com",
+            "name": "Daniel Thornburgh",
+            "username": "mysterymath"
+          },
+          "committer": {
+            "email": "mysterymath@gmail.com",
+            "name": "Daniel Thornburgh",
+            "username": "mysterymath"
+          },
+          "distinct": true,
+          "id": "72e337959d136a5971ad1c9c2eee7e933df0f2b2",
+          "message": "[Breaking] Use shifted PETSCII by default\n\nThe spirit of the C standard is the implementation do whatever\ntranslation is necessary to members of the execution character set to\nhave them stored or displayed properly on the target system. For\ncommodore targets, this really does mean that we should follow CC65 and\nuse the shifted character set.\n\nSince our execution character set is always ASCII, we do need to swap\nuppercase and lowercase characters at runtime. While the code to do this\nisn't *that* horrible, its overhead is the main reason we haven't done\nthus until now.\n\nHowever, preliminary design work for stdio provided a way to do this by\ndefault, while still allowing the user to turn it off. The default\n__char_conv implementation for commodore does this translation and pulls\nin a constructor that establishes shifted PETSCII at program start. If\nthe user provides their own __char_conv, the constructor will never be\npulled in, and they can use their own compile-time mechanisms to ensure\nthe printed strings are already in PETSCII as appropriate.",
+          "timestamp": "2024-01-31T10:14:18-08:00",
+          "tree_id": "41ad7523f27aad6ff1b47ce4d99e7fa489ad9e8b",
+          "url": "https://github.com/llvm-mos/llvm-mos-sdk/commit/72e337959d136a5971ad1c9c2eee7e933df0f2b2"
+        },
+        "date": 1706726086942,
         "tool": "googlecpp",
         "benches": [
           {
