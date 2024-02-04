3ea0141d37f05804574a931877f83bac029a7feb
Daniel Thornburgh <mysterymath@gmail.com>
2024-02-03 19:59:12 -0800

    Adjust to nonstandard _strto

--- SingleSource/UnitTests/strtoi.c
+++ SingleSource/UnitTests/strtoi.c
@@ -14,64 +14,64 @@ int main(void) {
   char tricky[] = "+0xz";
   errno = 0;
   /* basic functionality */
-  assert(strtoi("123", NULL, 10) == 123);
+  assert(_strtoi("123", NULL, 10) == 123);
   /* proper detecting of default base 10 */
-  assert(strtoi("456", NULL, 0) == 456);
+  assert(_strtoi("456", NULL, 0) == 456);
   /* proper functioning to smaller base */
-  assert(strtoi("14", NULL, 8) == 12);
+  assert(_strtoi("14", NULL, 8) == 12);
   /* proper autodetecting of octal */
-  assert(strtoi("016", NULL, 0) == 14);
+  assert(_strtoi("016", NULL, 0) == 14);
   /* proper autodetecting of hexadecimal, lowercase 'x' */
-  assert(strtoi("0xFF", NULL, 0) == 255);
+  assert(_strtoi("0xFF", NULL, 0) == 255);
   /* proper autodetecting of hexadecimal, uppercase 'X' */
-  assert(strtoi("0Xa1", NULL, 0) == 161);
+  assert(_strtoi("0Xa1", NULL, 0) == 161);
   /* proper handling of border case: 0x followed by non-hexdigit */
-  assert(strtoi(tricky, &endptr, 0) == 0);
+  assert(_strtoi(tricky, &endptr, 0) == 0);
   assert(endptr == tricky + 2);
   /* proper handling of border case: 0 followed by non-octdigit */
-  assert(strtoi(tricky, &endptr, 8) == 0);
+  assert(_strtoi(tricky, &endptr, 8) == 0);
   assert(endptr == tricky + 2);
   /* errno should still be 0 */
   assert(errno == 0);
   /* correctly decoding zero */
-  assert(strtoi("0", &endptr, 0) == 0);
+  assert(_strtoi("0", &endptr, 0) == 0);
   assert(*endptr == '\0');
   assert(errno == 0);
   /* overflowing subject sequence must still return proper endptr */
-  assert(strtoi(overflow, &endptr, 36) == INT_MIN);
+  assert(_strtoi(overflow, &endptr, 36) == INT_MIN);
   assert(errno == ERANGE);
   assert((endptr - overflow) == 53);
   /* same for positive */
   errno = 0;
-  assert(strtoi(overflow + 1, &endptr, 36) == INT_MAX);
+  assert(_strtoi(overflow + 1, &endptr, 36) == INT_MAX);
   assert(errno == ERANGE);
   assert((endptr - overflow) == 53);
   /* testing skipping of leading whitespace */
-  assert(strtoi(" \n\v\t\f789", NULL, 0) == 789);
+  assert(_strtoi(" \n\v\t\f789", NULL, 0) == 789);
   /* testing conversion failure */
-  assert(strtoi(overflow, &endptr, 10) == 0);
+  assert(_strtoi(overflow, &endptr, 10) == 0);
   assert(endptr == overflow);
   endptr = NULL;
-  assert(strtoi(overflow, &endptr, 0) == 0);
+  assert(_strtoi(overflow, &endptr, 0) == 0);
   assert(endptr == overflow);
   /* TODO: These tests assume two-complement, but conversion should work */
   /* for one-complement and signed magnitude just as well. Anyone having */
   /* a platform to test this on?                                         */
   errno = 0;
   /* testing "even" overflow, i.e. base is power of two */
-  assert(strtoi("32767", NULL, 0) == 0x7fff);
+  assert(_strtoi("32767", NULL, 0) == 0x7fff);
   assert(errno == 0);
   errno = 0;
-  assert(strtoi("32768", NULL, 0) == INT_MAX);
+  assert(_strtoi("32768", NULL, 0) == INT_MAX);
   assert(errno == ERANGE);
   errno = 0;
-  assert(strtoi("-32767", NULL, 0) == (int)0x8001);
+  assert(_strtoi("-32767", NULL, 0) == (int)0x8001);
   assert(errno == 0);
   errno = 0;
-  assert(strtoi("-32768", NULL, 0) == INT_MIN);
+  assert(_strtoi("-32768", NULL, 0) == INT_MIN);
   assert(errno == 0);
   errno = 0;
-  assert(strtoi("-32769", NULL, 0) == INT_MIN);
+  assert(_strtoi("-32769", NULL, 0) == INT_MIN);
   assert(errno == ERANGE);
   return 0;
 }
--- SingleSource/UnitTests/strtosc.c
+++ SingleSource/UnitTests/strtosc.c
@@ -14,56 +14,56 @@ int main(void) {
   char tricky[] = "+0xz";
   errno = 0;
   /* basic functionality */
-  assert(strtosc("123", NULL, 10) == 123);
+  assert(_strtosc("123", NULL, 10) == 123);
   /* proper functioning to smaller base */
-  assert(strtosc("14", NULL, 8) == 12);
+  assert(_strtosc("14", NULL, 8) == 12);
   /* proper autodetecting of octal */
-  assert(strtosc("016", NULL, 0) == 14);
+  assert(_strtosc("016", NULL, 0) == 14);
   /* proper handling of border case: 0x followed by non-hexdigit */
-  assert(strtosc(tricky, &endptr, 0) == 0);
+  assert(_strtosc(tricky, &endptr, 0) == 0);
   assert(endptr == tricky + 2);
   /* proper handling of border case: 0 followed by non-octdigit */
-  assert(strtosc(tricky, &endptr, 8) == 0);
+  assert(_strtosc(tricky, &endptr, 8) == 0);
   assert(endptr == tricky + 2);
   /* errno should still be 0 */
   assert(errno == 0);
   /* correctly decoding zero */
-  assert(strtosc("0", &endptr, 0) == 0);
+  assert(_strtosc("0", &endptr, 0) == 0);
   assert(*endptr == '\0');
   assert(errno == 0);
   /* overflowing subject sequence must still return proper endptr */
-  assert(strtosc(overflow, &endptr, 36) == SCHAR_MIN);
+  assert(_strtosc(overflow, &endptr, 36) == SCHAR_MIN);
   assert(errno == ERANGE);
   assert((endptr - overflow) == 53);
   /* same for positive */
   errno = 0;
-  assert(strtosc(overflow + 1, &endptr, 36) == SCHAR_MAX);
+  assert(_strtosc(overflow + 1, &endptr, 36) == SCHAR_MAX);
   assert(errno == ERANGE);
   assert((endptr - overflow) == 53);
   /* testing conversion failure */
-  assert(strtosc(overflow, &endptr, 10) == 0);
+  assert(_strtosc(overflow, &endptr, 10) == 0);
   assert(endptr == overflow);
   endptr = NULL;
-  assert(strtosc(overflow, &endptr, 0) == 0);
+  assert(_strtosc(overflow, &endptr, 0) == 0);
   assert(endptr == overflow);
   /* TODO: These tests assume two-complement, but conversion should work */
   /* for one-complement and signed magnitude just as well. Anyone having */
   /* a platform to test this on?                                         */
   errno = 0;
   /* testing "even" overflow, i.e. base is power of two */
-  assert(strtosc("127", NULL, 0) == 0x7f);
+  assert(_strtosc("127", NULL, 0) == 0x7f);
   assert(errno == 0);
   errno = 0;
-  assert(strtosc("128", NULL, 0) == SCHAR_MAX);
+  assert(_strtosc("128", NULL, 0) == SCHAR_MAX);
   assert(errno == ERANGE);
   errno = 0;
-  assert(strtosc("-127", NULL, 0) == (signed char)0x81);
+  assert(_strtosc("-127", NULL, 0) == (signed char)0x81);
   assert(errno == 0);
   errno = 0;
-  assert(strtosc("-128", NULL, 0) == SCHAR_MIN);
+  assert(_strtosc("-128", NULL, 0) == SCHAR_MIN);
   assert(errno == 0);
   errno = 0;
-  assert(strtosc("-129", NULL, 0) == SCHAR_MIN);
+  assert(_strtosc("-129", NULL, 0) == SCHAR_MIN);
   assert(errno == ERANGE);
   return 0;
 }
